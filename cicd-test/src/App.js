c3810f5af75f7dc8171003a0b6543087e2897036
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2023-08-13 13:42:04 +0100

    Hello World with Learn react link

--- cicd-test/src/App.js
+++ cicd-test/src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" // You can change this link if you want the "Learn More" to go somewhere else
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
